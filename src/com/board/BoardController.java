6ea08984635c76d7ee9edd99490a86a546524fb9
seojaemin <dydgns11@naver.com>
2019-03-19 12:55:59 +0900

    나도 주석추가 서재민

--- src/com/board/BoardController.java
+++ src/com/board/BoardController.java
@@ -18,7 +18,7 @@ public class BoardController implements Controller {
 	String crud =null;
 	Logger logger =  Logger.getLogger(BoardController.class);
 	
-
+	//나도추가
 	@Override
 	public String execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		String path=null;
